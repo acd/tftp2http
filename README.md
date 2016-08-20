eaf99704d7a90b5c8881926f8e311ff9c81875e0
Andrew Danforth <acd@weirdness.net>
2016-08-19 23:47:34 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# tftp2http
-TFTP to HTTP proxy
+## tftp2http
+
+So it's 2016 and I decided to teach myself Go, but I wanted to write something that could actually prove useful.  TFTP is still used for a variety of reasons, and I've occasionally wished I could skip TFTP and use an HTTP server instead.  Thus, tftp2http...
+
+This daemon transforms TFTP requests to HTTP requests on the fly.  TFTP RRQs are turned in to HTTP GETs, and WRQs become HTTP PUTs.  It uses the excellent [Go TFTP library](https://github.com/pin/tftp) developed by [Dmitri Popov](https://github.com/pin).  
+
+### Building
+
+Building the daemon is currently an exercise left to the reader; I've not done a release and provided binaries.  Let me know if you'd like me to.
+
+### Can I see it work?
+
+Lets proxy all requests to Google!
+
+```
+$ tftp2http -listen=:10000 http://www.google.com/
+2016/08/19 23:35:37 proxying TFTP requests on :10000 to http://www.google.com/
+```
+
+Now, in another shell, let's make a request:
+
+```
+$ tftp 127.0.0.1 10000
+tftp> bin
+tftp> get index.html
+Received 11158 bytes in 0.0 seconds
+```
+
+And back on the proxy we see:
+
+```
+2016/08/19 23:41:39 {127.0.0.1:59102} received RRQ 'index.html'
+2016/08/19 23:41:39 {127.0.0.1:59102} completed RRQ 'index.html' bytes:11158,duration:80.241477ms
+```
+
+### TFTP details
+
+The TFTP server supports the `tsize` and `blksize` options.  The RRQ `tsize` option will not be acknowledged if the HTTP GET response lacks the Content-Length header.
+
+### HTTP details
+
+HTTP requests include headers to convey the original request source.  They are:
+
+* X-Forwarded-For: source IP address and port
+* X-Forwarded-Proto: tftp
+* Forwarded: per [RFC 7239](https://tools.ietf.org/html/rfc7239)
+
+HTTP PUTs use chunked encoding to transmit the request body as it is received from the TFTP client.  Content-Length is not provided even if the client included it as part of its WRQ options.
---
+++ tftp2http.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+	"log"
+
+	"github.com/acd/tftp"
+)
+
+var (
+	listenAddr  = flag.String("listen", ":69", "Listen address and port")
+	httpTimeout = flag.Int("http-timeout", 5, "HTTP timeout")
+	httpMaxIdle = flag.Int("http-max-idle", 10, "HTTP max idle connections")
+	tftpTimeout = flag.Float64("tftp-timeout", 1, "TFTP timeout")
+	tftpRetries = flag.Int("tftp-retries", 5, "TFTP retries")
+	serverUrl   *url.URL
+)
+
+var client *http.Client
+
+func processFlags() error {
+	flag.Parse()
+
+	var err error
+	if len(flag.Args()) != 1 {
+		return fmt.Errorf("must provide server")
+	}
+	server := flag.Args()[0]
+	serverUrl, err = url.Parse(server)
+	if err != nil {
+		return fmt.Errorf("url: error parsing: '%s'", server)
+	}
+	if serverUrl.Scheme != "http" && serverUrl.Scheme != "https" {
+		return fmt.Errorf("url: invalid scheme: '%s'", serverUrl.Scheme)
+	}
+	if len(serverUrl.Host) == 0 {
+		return fmt.Errorf("url: host must be provided")
+	}
+
+	return nil
+}
+
+func setForwardedHeader(req *http.Request, from string) {
+	req.Header.Add("X-Forwarded-From", from)
+	req.Header.Add("X-Forwarded-Proto", "tftp")
+	req.Header.Add("Forwarded", "for=\""+from+"\",proto=tftp")
+}
+
+func readHandler(filename string, rf io.ReaderFrom) error {
+	raddr := rf.(tftp.OutgoingTransfer).RemoteAddr()
+	from := raddr.String()
+
+	log.Printf("{%s} received RRQ '%s'", from, filename)
+
+	u := *serverUrl
+	u.Path += filename
+
+	start := time.Now()
+	var n int64
+	defer func() {
+		elapsed := time.Since(start)
+		log.Printf("{%s} completed RRQ '%s' bytes:%d,duration:%s", from, filename, n, elapsed)
+	}()
+
+	req, err := http.NewRequest("GET", u.String(), nil)
+	setForwardedHeader(req, from)
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Printf("{%s} error on HTTP GET: %s", from, err)
+		return err
+	}
+	defer res.Body.Close()
+
+	switch res.StatusCode {
+	case 200:
+		break
+	case 404:
+		return fmt.Errorf("File not found")
+	default:
+		log.Printf("{%s} unexpected response status code is: %d", from, res.StatusCode)
+		return fmt.Errorf("Unexpected response code: %d", res.StatusCode)
+	}
+
+	if res.ContentLength >= 0 {
+		rf.(tftp.OutgoingTransfer).SetSize(res.ContentLength)
+	}
+
+	n, err = rf.ReadFrom(res.Body)
+	if err != nil {
+		log.Printf("{%s} ReadFrom returned error: %s", from, err)
+		return err
+	}
+
+	return nil
+}
+
+func writeHandler(filename string, wt io.WriterTo) error {
+	raddr := wt.(tftp.IncomingTransfer).RemoteAddr()
+	from := raddr.String()
+
+	log.Printf("{%s} received WRQ '%s'", filename, from)
+
+	u := *serverUrl
+	u.Path += filename
+
+	start := time.Now()
+	var n int64
+	defer func() {
+		elapsed := time.Since(start)
+		log.Printf("{%s} completed WRQ '%s' bytes:%d,duration:%s", from, filename, n, elapsed)
+	}()
+
+	r, w := io.Pipe()
+	done := make(chan int64)
+	go func() {
+		defer w.Close()
+		n, _ := wt.WriteTo(w)
+		done <- n
+	}()
+
+	req, err := http.NewRequest("PUT", u.String(), r)
+	setForwardedHeader(req, from)
+	req.Header.Add("Content-Type", "application/octet-stream")
+	req.ContentLength = -1
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Printf("error on HTTP PUT: %s", err)
+		return err
+	}
+	defer res.Body.Close()
+
+	n = <-done
+	return nil
+}
+
+func main() {
+	if err := processFlags(); err != nil {
+		fmt.Println(err)
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	client = &http.Client{
+		Transport: &http.Transport{
+			MaxIdleConnsPerHost: *httpMaxIdle,
+		},
+		Timeout: time.Duration(*httpTimeout) * time.Second,
+	}
+
+	s := tftp.NewServer(readHandler, writeHandler)
+	s.SetTimeout(time.Duration(*tftpTimeout) * time.Second)
+	s.SetRetries(*tftpRetries)
+	// s.SetBackoff(func (int) time.Duration { return 0 })
+
+	log.Printf("proxying TFTP requests on %s to %s", *listenAddr, serverUrl)
+	err := s.ListenAndServe(*listenAddr)
+	if err != nil {
+		log.Fatalf("server: %v\n", err)
+	}
+}
